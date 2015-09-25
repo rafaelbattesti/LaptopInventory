a0924033b23dfe343a46d910f712a4704eb6889d
rafaelbattesti <rafaelbattesti@battestimacbook.local>
2015-09-25 00:20:35 -0400

    Connection with DB. Needs proper error logging

--- com/rafaelbattesti/dataaccess/DataAccess.java
+++ com/rafaelbattesti/dataaccess/DataAccess.java
@@ -10,8 +10,61 @@ import java.sql.*;
  */
 public class DataAccess {
 	
-	//Attribute to hold the connection
+	// JDBC 
+	private final String JDBC_URL = "jdbc:mysql://localhost/Laptopdb";
+	private final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
 	
+	// Database credentials
+	private final String DB_USER = "root";
+	private final String DB_PASS = "1111";
 	
+	// Holds a connection object created by the connect() method
+	private Connection conn;
+	
+	/**
+	 * Connects to database
+	 */
+	public void connect() {
+		
+		try {
+			
+			// Load driver
+			Class.forName(JDBC_DRIVER);
+			
+			// Create connection with DriverManager
+			conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS);
+			
+		} catch (ClassNotFoundException e) {
+			
+			// TODO Log error properly - Could not find Driver class
+			e.printStackTrace();
+			
+		} catch (SQLException e) {
+			
+			// TODO Log error properly - Could not connect to DB
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	/**
+	 * Disconnects from database
+	 */
+	public void disconnect() {
 
+		try {
+			
+			// Closes connection if one exists
+			if (conn != null) {
+				conn.close();
+			}
+			
+		} catch (SQLException e) {
+			
+			// TODO Log error properly - Could not close connection
+			e.printStackTrace();
+		}
+
+	}
 }
