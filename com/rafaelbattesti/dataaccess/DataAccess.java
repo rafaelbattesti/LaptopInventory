bbcab612e33aca6543cb5ca5d51dc19bee12f094
rafaelbattesti <rafaelbattesti@battestimacbook.local>
2015-09-26 01:03:32 -0400

    Error logging. Need write logs to file.

--- com/rafaelbattesti/dataaccess/DataAccess.java
+++ com/rafaelbattesti/dataaccess/DataAccess.java
@@ -2,29 +2,51 @@ package com.rafaelbattesti.dataaccess;
 
 // Import the related packages
 import java.sql.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 /**
- * This class is responsible for handling DB connection and queries
- * @author rafaelbattesti
+ * This class is responsible for handling DB connection, queries and logging errors.
+ * @author rafaelbattesti - 991382266
  *
  */
 public class DataAccess {
 	
 	// JDBC 
-	private final String JDBC_URL = "jdbc:mysql://localhost/Laptopdb";
-	private final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	private final String JDBC_URL       = "jdbc:mysql://localhost/Laptopdb";
+	private final String JDBC_DRIVER    = "com.mysql.jdbc.Drive";
 	
 	// Database credentials
-	private final String DB_USER = "root";
-	private final String DB_PASS = "1111";
+	private final String DB_USER        = "root";
+	private final String DB_PASS        = "1111";
+	
+	// Err messages
+	private final String ERR_DRIVER     = "JDBC Driver not found";
+	private final String ERR_CONNECT    = "Could not connect to database";
+	private final String ERR_DISCONNECT = "Could not disconnect from database";
 	
 	// Holds a connection object created by the connect() method
 	private Connection conn;
+	
+	// Holds errors
+	private String log;
+	
+	/**
+	 * @return
+	 */
+	public String getLog () {
+		return log;
+	}
 	
 	/**
 	 * Connects to database
 	 */
 	public void connect() {
+		
+		// Resets the log attribute
+		log = null;
 		
 		try {
 			
@@ -35,17 +57,10 @@ public class DataAccess {
 			conn = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS);
 			
 		} catch (ClassNotFoundException e) {
-			
-			// TODO Log error properly - Could not find Driver class
-			e.printStackTrace();
-			
+			log = logErr(e, ERR_DRIVER);
 		} catch (SQLException e) {
-			
-			// TODO Log error properly - Could not connect to DB
-			e.printStackTrace();
+			log = logErr(e, ERR_CONNECT);
 		}
-		
-		
 	}
 	
 	/**
@@ -53,6 +68,9 @@ public class DataAccess {
 	 */
 	public void disconnect() {
 
+		// Resets the log attribute
+		log = null;
+		
 		try {
 			
 			// Closes connection if one exists
@@ -60,11 +78,31 @@ public class DataAccess {
 				conn.close();
 			}
 			
-		} catch (SQLException e) {
-			
-			// TODO Log error properly - Could not close connection
-			e.printStackTrace();
+		} catch (SQLException e) {	
+			log = logErr(e, ERR_DISCONNECT);
 		}
 
 	}
+	
+	/**
+	 * Assigns log message to object's attribute
+	 * @param e
+	 * @param errMessage
+	 * @return log message
+	 */
+	private String logErr (Exception e, String errMessage) {
+		
+		// Sets the date of the error
+		DateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		String date = df.format(new Date());
+		
+		// Logs main SQLException attributes for debugging or only the errMessage
+		if (e instanceof SQLException) {
+			SQLException ex = (SQLException) e;
+			return date + ":::" + errMessage + ":::" + ex.getSQLState() + ":::" + ex.getErrorCode();
+		} else {
+			return date + ":::" + errMessage;
+		}
+	}
+	
 }
